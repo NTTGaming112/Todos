f714240daf3dd96e8cc173403e525ff704c9d9a1
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-11-08 20:25:27 +0700

    api add todo

--- src/app/todos/todo.tsx
+++ src/app/todos/todo.tsx
@@ -32,6 +32,18 @@ function Todos() {
       todo: newTodo,
       completed: false,
     };
+    fetch('https://dummyjson.com/todos/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        todo: newTodo,
+        completed: false,
+        userId: 5,
+      })
+    })
+    .then(res => res.json())
+    .then(console.log);
+
     setTodos([...todos, newTodoItem]);
     setNewTodo("");
     setShowInput(false);
